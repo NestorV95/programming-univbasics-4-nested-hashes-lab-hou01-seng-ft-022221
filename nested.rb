00bfa5ae12cc21f360e0feb79758c06bd28f8e31
Nestor Venegas <nestor.venegas95@gmail.com>
2021-02-05 02:19:38 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,8 @@ def hopper
         :languages => ["C"]
       }
     }
-   
+   end 
+   programmer_hash[:grace_hopper]=
 end
 
 def alan_kay_is_known_for
@@ -36,7 +37,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+ programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -125,5 +126,4 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
 end
