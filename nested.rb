c909c802aa66a67fd4f1858e79fc4b0a78b066b5
Nestor Venegas <nestor.venegas95@gmail.com>
2021-02-05 02:35:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -106,7 +106,9 @@ def changing_alan
         :languages => ["C"]
       }
     }
-programmer_hash[:alan_kay][:known_for]="GUI"
+   programmer_hash[:alan_kay][:known_for]="GUI"
+  end
+programmer_hash
 end
 
 def adding_to_dennis
